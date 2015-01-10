561aae922aec4fbc4edbe399de5fc89553f490f5
Eric Wang <kuchaguangjie@gmail.com>
2015-01-10 13:23:50 +0800

    ...

--- install.sh
+++ install.sh
@@ -1,12 +1,22 @@
 #! /bin/bash
 # Tip: this file must be executed in it's direct containing folder,
 
+## locations - could modify, modify it before install if need,
 bin_dir="/usr/bin"
 
+## locations - can't modify,
+pygtrans_config_dir="$HOME/.config/pygtrans"
+
+## command - via "ln -s"
 ln -sf "$PWD/main.py" "$bin_dir/pygtrans"
 if [ $? -eq 0 ]; then
-	echo "Done. Use 'pygtrans' as command."
+	echo "Done."
+	echo "Use 'pygtrans' as command."
 fi
+
+## config file
+mkdir -p $pygtrans_config_dir
+cp ./template/config.ini.original $pygtrans_config_dir/config.ini
 
 exit 0
 
--- main.py
+++ main.py
@@ -4,12 +4,37 @@
 import sys
 import urllib2
 from BeautifulSoup import BeautifulSoup
+import ConfigParser
+from os.path import expanduser
 
-version = "v0.1" + " beta1"
+version = "v0.1" + " beta2"
+
+# read config
+googleDomain = "google.com"
+targetLang = "en"
+srcLang = "auto"
+
+home = expanduser("~")
+config = ConfigParser.RawConfigParser()
+config.read(home + "/.config/pygtrans/config.ini")
+
+if config.has_option("basic","google_domain"):
+	tmpDomain = config.get("basic", "google_domain")
+	if tmpDomain and (not tmpDomain.isspace()):
+		googleDomain = tmpDomain
+
+if config.has_option("basic","target_language"):
+	tmpTargetLang = config.get("basic", "target_language")
+	if tmpTargetLang and (not tmpTargetLang.isspace()):
+		targetLang = tmpTargetLang
+
+if config.has_option("basic","source_language"):
+	tmpSrcLang = config.get("basic", "source_language")
+	if tmpSrcLang and (not tmpSrcLang.isspace()):
+		srcLang = tmpSrcLang
+
 ## param check
 key = ""
-targetLang = "cn"
-srcLang = "auto"
 if len(sys.argv) < 2:
 	print("Please use following formats:\n\t%s" % ( "gtrans <input_string> [<target_language>] [<source_language>]"))
 	print("\t%s" % ("gtrans (-v | -version)"))
@@ -29,7 +54,7 @@ else:
 resultId = "result_box"
 userAgent = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
 headers = {'User-Agent':userAgent}
-url = "http://translate.google.cn/?langpair=" + srcLang + "|" + targetLang + "&text=" + key
+url = "http://translate." + googleDomain + "/?langpair=" + srcLang + "|" + targetLang + "&text=" + key
 page = urllib2.urlopen(urllib2.Request(url, None, headers))
 
 ## result parse
